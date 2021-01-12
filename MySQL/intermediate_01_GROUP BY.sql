df7e9a36a303ce50c3baa73f850664efc587eb75
Seohyeon Jeong <seohyeon.jeong1214@gmail.com>
2021-01-13 06:50:22 +0900

    Update intermediate_01_GROUP BY.sql

--- MySQL/intermediate_01_GROUP BY.sql
+++ MySQL/intermediate_01_GROUP BY.sql
@@ -39,7 +39,10 @@ FROM city
 SELECT count(city)- count(DISTINCT city)
 FROM station
 
-/* We define an employee's total earnings to be their monthly salaryXmonths worked, 
+
+-- HackerRank) TOP Earners
+/* 
+We define an employee's total earnings to be their monthly salaryXmonths worked, 
 and the maximum total earnings to be the maximum total earnings for any employee in the Employee table. 
 Write a query to find the maximum total earnings for all employees as well as the total number of employees who have maximum total earnings.
 Then print these values as 2 space-separated integers.*/
