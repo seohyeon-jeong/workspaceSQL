61eee6cb7b2659c86b11799d1075ed0d70f8ce3d
Seohyeon Jeong <seohyeon.jeong1214@gmail.com>
2021-02-02 20:29:00 +0900

    sql basic 복습 2회차 완료

--- MySQL/basic_01_WHERE.sql
+++ MySQL/basic_01_WHERE.sql
@@ -10,9 +10,13 @@ FROM Customers
 -- LIKE 보다는 = 이 더 빠르니 정확한 경우에는 = 써주기 
 -- WHERE country LIKE 'B______' -- 특정 문자의 개수를 _로 표기 
 
+-- WHERE 
+
 -- WHERE discount LIKE '50\%'
 -- WHERE discount LIKE '__\%' -- 2자리 퍼센트 찾기 
 -- % 자체를 예약어가 아닌 문자로 사용하고 싶은 경우(=escape)에는 \%로 역슬레시 추가
+
+-- WHERE country NOT LIKE 'an%' -- an으로 시작하지 않는~ 
 
 -- WHERE country =  'Germany' OR country = 'France'
 -- WHERE country IN ('Germany', 'France')
@@ -27,3 +31,6 @@ FROM Customers
 -- LIMIT 10
 
 -- SELECT DISTINCT ~ -- 중복값 제외 
+-- SELECT DISTINCT country FROM Customers
+
+-- WHERE customerID <> 1 -- 특정 값 제외 
--- MySQL/basic_02_ORDER BY.sql
+++ MySQL/basic_02_ORDER BY.sql
@@ -1,7 +1,6 @@
 -- 데이터 정렬하기
 SELECT * 
 FROM Customers
-WHERE 
 ORDER BY customerID DESC -- 내림차순 
 -- ASC 오름차순(default) : 작은 것부터 -> 높은 것
 -- 저장되어 있는 데이터의 순서를 변경하지 않는다. 뷰의 순서만 변경
@@ -25,5 +24,3 @@ ORDER BY employee_id
 
 -- greater than, higher than : 초과 
 -- less than : 미만
-
-
